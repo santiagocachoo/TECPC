53597819b97f0d2bd6d90b0df63247f0b1692e47
santiago <a01255853@tec.mx>
2024-10-12 18:42:13 -0700

    añadir inputs a funcion de magnitud de desplazamiento

--- proyecto_integrador/magnitud_desplazamiento.py
+++ proyecto_integrador/magnitud_desplazamiento.py
@@ -1,7 +1,30 @@
 import math as m
 
-def magnitud_desplazamiento(pix, pfx, piy, pfy):                    # toma punto inicial y punto final de x y y
-    desplazamiento_x = pfx - pix                                    # calcula desplazamiento de x
-    desplazamiento_y = pfy - piy                                    # calcula desplazamiento de y
-    magnitud = m.sqrt(desplazamiento_x**2 + desplazamiento_y**2)    # calcula magnitud con teorema de pitagoras
-    return magnitud                                                 # devuelve magnitud
+def magnitud_desplazamiento():
+    """
+    esta función calcula la magnitud de desplazamiento tomando las coordenadas del punto inicial y punto final
+    pide los datos para cada punto y despues calcula el desplazamiento para el eje "x" y "y" 
+    finalmente calcula la magnitud usando el teorema de pitagoras con los desplazamientos
+    """
+    # función para manejar el ingreso de datos 
+    def input_float(mensaje):
+        while True:
+            try:
+                valor = float(input(mensaje))
+                return valor
+            except ValueError:
+                print('Por favor, ingrese un número válido.')
+
+    # pedir puntos iniciales y finales
+    punto_inicial_x = input_float('Ingrese el punto inicial en x: ')
+    punto_inicial_y = input_float('Ingrese el punto inicial en y: ')
+    punto_final_x = input_float('Ingrese el punto final en x: ')
+    punto_final_y = input_float('Ingrese el punto final en y: ')
+
+    # calcular desplazamientos
+    desplazamiento_x = punto_final_x - punto_inicial_x
+    desplazamiento_y = punto_final_y - punto_inicial_y
+
+    # calcular magnitud
+    magnitud = m.sqrt(desplazamiento_x**2 + desplazamiento_y**2)
+    return magnitud
